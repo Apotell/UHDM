311d0604fdc25d15101b3ab69ec08ea2e42d1827
Garik Petrosyan <petrosyangarik00@gmail.com>
2025-12-29 00:30:50 +0400

    Milestone 2: add read-only BaseClass inspection bindings

--- python/pybind11/bindings/bind_baseclass.cpp
+++ python/pybind11/bindings/bind_baseclass.cpp
@@ -1,7 +1,16 @@
 #include <pybind11/pybind11.h>
+#include <uhdm/BaseClass.h>
 
 namespace py = pybind11;
 
 void bind_baseclass(py::module_& m) {
-    // Placeholder for bind_baseclass
+    py::class_<uhdm::BaseClass, std::unique_ptr<uhdm::BaseClass, py::nodelete>>(m, "BaseClass")
+        .def("getUhdmId", &uhdm::BaseClass::getUhdmId)
+        .def("getFile", &uhdm::BaseClass::getFile)
+        .def("getStartLine", &uhdm::BaseClass::getStartLine)
+        .def("getStartColumn", &uhdm::BaseClass::getStartColumn)
+        .def("getEndLine", &uhdm::BaseClass::getEndLine)
+        .def("getEndColumn", &uhdm::BaseClass::getEndColumn)
+        .def("getVpiType", &uhdm::BaseClass::getVpiType)
+        .def("getUhdmType", &uhdm::BaseClass::getUhdmType);
 }
