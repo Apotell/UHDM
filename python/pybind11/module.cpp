dcf1a75d4afbf730635850205bec34622a572d34
Garik Petrosyan <petrosyangarik00@gmail.com>
2025-12-29 00:11:23 +0400

    Milestone 2: add scaffolding for model inspection bindings

--- python/pybind11/module.cpp
+++ python/pybind11/module.cpp
@@ -4,9 +4,15 @@ namespace py = pybind11;
 
 // Forward declarations
 void bind_serializer(py::module& m);
+void bind_any(py::module& m);
+void bind_baseclass(py::module& m);
+void bind_enums(py::module& m);
 
 PYBIND11_MODULE(pyuhdm, m) {
     m.doc() = "UHDM Python bindings";
 
     bind_serializer(m);
+    bind_any(m);
+    bind_baseclass(m);
+    bind_enums(m);
 }
